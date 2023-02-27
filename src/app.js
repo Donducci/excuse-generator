413313d75ae0062b5be59d1ba51070e836139336
Donald Uzochukwu <donduchi.ceejay@gmail.com>
2023-02-27 15:47:50 +0000

    my complete excuse generator project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const paragraph = document.querySelector("#the-excuse");
+  let pronoun = ["A", "The"];
+  let subject = ["teacher", "goat", "cat", "driver", "mechanic"];
+  let action = ["took my", "eat my", "tore my", "drove my", "broke my"];
+  let object = ["homework", "banana", "clothe", "car", "ratchet"];
+  let location = [
+    "in the classroom",
+    "in my farm",
+    "in my house",
+    "im my garage",
+    "in my workshop"
+  ];
+  let proindx = Math.floor(Math.random() * pronoun.length);
+  let subindx = Math.floor(Math.random() * subject.length);
+  let actindx = Math.floor(Math.random() * action.length);
+  let objindx = Math.floor(Math.random() * object.length);
+  let locindx = Math.floor(Math.random() * location.length);
+  let excuse =
+    pronoun[proindx] +
+    " " +
+    subject[subindx] +
+    " " +
+    action[actindx] +
+    " " +
+    object[objindx] +
+    " " +
+    location[locindx];
+  paragraph.textContent = excuse;
 };
